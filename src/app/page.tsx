be9b5a5db26f65f7dfefbbdc36f1beb4dc96294c
TL <tlgamer.gaming@gmail.com>
2025-12-15 12:56:04 +0200

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,85 @@
-import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardFooter, CardTitle, CardAction, CardDescription, CardContent, } from "@/components/ui/card";
+import { Typewriter } from "@/components/ui/typewriter-text"
+import DarkVeil from '@/components/DarkVeil';
+
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <div className="">
+      {/* background */}
+      <div className="fixed inset-0 w-full h-full z-0">
+        <DarkVeil hueShift={380} />
+      </div>
+
+      <main className="min-h-screen min-w-screen relative z-10">
+        <nav className="flex w-screen justify-center pt-8 fixed z-50">
+          <div className="flex flex-wrap items-center justify-center border mx-4 px-6 py-4 rounded-full text-white text-sm gap-4">
+            <a href="https://timilindblom.com">
+              <img
+                className=""
+                src="/next.svg"
+                alt="Timi"
+                width={40}
+                height={40}
+              />
+            </a>
+            <div className="flex items-center gap-4 md:gap-6 md:ml-7">
+              <a href="#" className="relative overflow-hidden h-6 group">
+                <span className="block group-hover:-translate-y-full transition-transform duration-300">About</span>
+                <span className="block absolute top-full left-0 group-hover:-translate-y-full transition-transform duration-300">About</span>
+              </a>
+              <a href="#" className="relative overflow-hidden h-6 group">
+                <span className="block group-hover:-translate-y-full transition-transform duration-300">Projects</span>
+                <span className="block absolute top-full left-0 group-hover:-translate-y-full transition-transform duration-300">Projects</span>
+              </a>
+            </div>
+
+            <div className="flex items-center gap-4 md:ml-10">
+              <button className="bg-white hover:shadow-[0px_0px_30px_7px] shadow-[0px_0px_30px_3px] hover:shadow-white/50 shadow-white/50 text-black px-4 py-2 rounded-full text-sm font-medium hover:bg-slate-100 transition duration-300">
+                Contact
+              </button>
+            </div>
+          </div>
+        </nav>
+
+        <div className='flex items-center pt-50 w-full'>
+
+          <div className='flex items-center max-w-3xl ml-[40%] -translate-x-1/2'>
+            <div className='w-96'>
+              <h2 className="text-xl font-medium text-white">I'm Timi Lindblom.</h2>
+              <Typewriter
+                text={["Developer", "CS Student", "Ambitious"]}
+                speed={100}
+                loop={true}
+                className="text-xl font-medium text-white"
+              />
+            </div>
+
+            <img
+              className=""
+              src="/timi-bw.PNG"
+              alt="Timi"
+              width={300}
+              height={300}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+          </div>
+
         </div>
+
+        <h1 className=" text-2xl font-bold text-white mt-8 text-center">About me</h1>
+
+
+        <h1 className=" text-2xl font-bold text-white mt-8 text-center">Projects</h1>
+
+
+
+
+
+
       </main>
     </div>
   );
