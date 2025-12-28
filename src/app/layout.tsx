86bf005f36e8dd6fe4a5a0e0f7c41ccba79df346
TL <tlgamer.gaming@gmail.com>
2025-12-28 17:05:49 +0200

    change of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Hi",
-  description: "lol",
+  title: "Timi Lindblom",
+  description: "Timi Lindblom, Developer, CS student.",
 };
 
 export default function RootLayout({
